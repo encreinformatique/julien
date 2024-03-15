a8a0588f945be420a6a6ae137e28fb14dce59289
Julien Devergnies <j.devergnies@outlook.com>
2024-03-16 00:27:58 +0100

    fixed : French text, couple of mishaps in mobile

--- resources/views/components/description.blade.php
+++ resources/views/components/description.blade.php
@@ -3,7 +3,7 @@
         <x-layout.h2>{{ __('presentation.title') }}</x-h2>
         <p class="my-3 mt-6">{!! __('presentation.me') !!}</p>
         <p class="my-3">{{ __('presentation.you') }}</p>
-        <nav class="flex justify-between mb-3">
+        <nav class="grid grid-cols-1 md:grid-cols-3 gap-2 mb-3">
             <a href="#contact" class="mr-4 underline hover:text-sky-600">{{ __('presentation.cta') }}</a>
             <a href="#abilities" class="mr-4 underline hover:text-sky-600">{{ __('presentation.abilities') }}</a>
             <a href="https://linkedin.com/in/jdevergnies" target="_blank" class="mx-4 hover:text-sky-600 flex">
